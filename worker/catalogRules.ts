786be31147bd1ddaf326231e6527bf58fb9081ab
marcioherlein <120120133+marcioherlein@users.noreply.github.com>
2026-08-13 19:33:59 -0300

    feat: add catalog matching rules

---
+++ worker/catalogRules.ts
@@ -0,0 +1,2 @@
+export const EXCLUDED_LISTING_TERMS = ['funda', 'protector', 'overgrip', 'pelota', 'bolso', 'mochila']
+export const HIGH_BRAND_EQUITY = ['nox', 'bullpadel', 'adidas', 'head', 'wilson', 'babolat']
